4457d9069d03005abefe290d20db4f01eed73422
Scott Morris <scootrmorris@gmail.com>
2021-11-30 07:55:22 -0800

    added saved features and labels

--- model.py
+++ model.py
@@ -6,21 +6,69 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle 
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
 
+# make sure to change filepath for images and unzip file
+
+# creating data path to training data
+DATADIR = "/Users/scottmorris/Desktop/model/Training"
+CATEGORIES = ["Black", "East Asian", "Indian", "Latino", "Middle Eastern", "Southeast Asian", "White"]
+
+for category in CATEGORIES: 
+    path = os.path.join(DATADIR, category) # path to each classification directory
+    for img in os.listdir(path): # loop through each image in each classification directory
+        img_array = cv2.imread(os.path.join(path, img)) 
+        plt.imshow(img_array, cmap='gray')
+        plt.show() 
+        break
+    break
+
+print(img_array) 
+
+training_data = []
+
+def create_training_data(): # looping through each folder
+    for category in CATEGORIES: 
+        path = os.path.join(DATADIR, category) 
+        class_num = CATEGORIES.index(category) # assigning number to each classification            
+        for img in os.listdir(path): # loop through each image in each folder
+            try:
+                img_array = cv2.imread(os.path.join(path, img)) 
+                training_data.append([img_array, class_num]) # add to training data
+            except Exception as e:
+                pass
+
+print(len(training_data)) # confirm training data is correct
+
+# shuffle data so not just learning from one label at a time
+random.shuffle(training_data)
+                
+X = [] # features
+y = [] # labels
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, 224, 224, 3)
+
+# use pickle to save data
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+# to reload data
 X = pickle.load(open("X.pickle","rb"))
 y = pickle.load(open("y.pickle","rb"))
 
 X = X/255.0
-
-# add convolutional & pooling layers
-x = layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")
-x = layers.MaxPooling2D(pool_size=(3, 3))(x)
-x = layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")
-x = layers.MaxPooling2D(pool_size=(3, 3))(x)
-x = layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")
-
-num_classes = 7
-outputs = layers.Dense(num_classes, activation="softmax")
 
 
 # create model object and layers
@@ -41,8 +89,10 @@ model.add(Dense(64)) # fully connected layer
 model.add(Dense(1)) # single output
 model.add(Activation('sigmoid')) 
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+             optimizer='adam', 
+             metrics=['accuracy'])
 
 # model training instructions
-model.fit(X, y, batch_size=20, epochs=10, validation_split=0.2)
+model.fit(X, y, batch_size=30, epochs=10, validation_split=0.2) # split data into training and validation sets  (20% validation)
 
